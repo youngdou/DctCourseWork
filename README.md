c8a892cc40497347b885966d8245b6fa2b7e8bd5
youngdou <yaoyongdou@gmail.com>
2016-11-12 19:57:47 +0800

    添加使用说明

--- README.md
+++ README.md
@@ -4,3 +4,10 @@
 ## 技术说明
 - 前端使用bootstrap框架
 - 使用jquery框架
+
+## quick start
+
+- Maven自动下载好依赖以后执行`mvn jetty:run`
+- 在浏览器直接访问`localhost:8080`或者`localhost:8080/index`即可进去首页
+
+> **注**：由于没有使用tomcat，所以`localhost:8080`即可进入首页，**无需添加项目名**
--- restaurant/src/main/java/hello/IndexController.java
+++ restaurant/src/main/java/hello/IndexController.java
@@ -9,11 +9,16 @@ import org.springframework.web.bind.annotation.RequestMapping;
  * Created by young on 16-11-2.
  */
 @Controller
-@RequestMapping("/index")
+@RequestMapping("/")
 public class IndexController {
 
     @GetMapping
     public String IndexPage(Model model) {
+        model.addAttribute("title", "首页");
+        return "index";
+    }
+    @GetMapping("/index")
+    public String IndexPage2(Model model) {
         model.addAttribute("title", "首页");
         return "index";
     }
